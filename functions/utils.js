c2799be65eb55086efb0fdc4dbb7db0db9863f6d
BEN MALLEM Yousra <Yousra.Ben_Mallem@bordeaux-inp.fr>
2021-01-23 22:17:13 +0100

    new version

---
+++ functions/utils.js
@@ -0,0 +1,70 @@
+const jsonTVA = require('./countryTVA.json')
+const tables = jsonTVA.countries
+const utils = require('./utils')
+
+module.exports.idRepo = function (id) {
+  if (id === undefined) {
+    return 'Unknown'
+  } else {
+    return id
+  }
+}
+
+module.exports.price = function (quantities, prices, country, discount = 'NO_DISCOUNT') {
+  let TVA = 0
+  tables.forEach(function (table) {
+    if (table.code === country) {
+      TVA = table.TVA
+    }
+  })
+  /* if (TVA == 0) {
+        next(err);
+    } */
+  const priceHT = (quantities[0] || 0) * (prices[0] || 0) + (quantities[1] || 0) * (prices[1] || 0)
+  const priceTTC = priceHT + priceHT * TVA / 100
+  const discountedPrice = utils.applydiscount(priceTTC, discount)
+  return discountedPrice
+}
+
+module.exports.progressiveDiscount = function (price) {
+  switch (true) {
+    case price >= 50000:
+      return (price - price * 15 / 100)
+    case price >= 10000 && price < 50000:
+      return (price - price * 10 / 100)
+    case price >= 7000 && price < 10000:
+      return (price - price * 7 / 100)
+    case price >= 5000 && price < 7000:
+      return (price - price * 5 / 100)
+    case price >= 1000 && price < 5000:
+      return (price - price * 3 / 100)
+    default: return price
+  }
+}
+
+module.exports.applydiscount = function (price, discount) {
+  switch (discount) {
+    case 'NO_DISCOUNT':
+      return price
+    case 'PROGRESSIVE_DISCOUNT':
+      return utils.progressiveDiscount(price)
+    case 'FLAT_DISCOUNT':
+      return price - price * 30 / 100
+    case 'FIXED_DISCOUNT':
+      return utils.fixedDiscount(price)
+    default:
+      return price
+  }
+}
+
+module.exports.fixedDiscount = function (price) {
+  switch (true) {
+    case price >= 1000:
+      return price - 200
+    case price >= 400 && price < 1000:
+      return price - 50
+    case price >= 100 && price < 400:
+      return price - 10
+    default: return price
+  }
+}
